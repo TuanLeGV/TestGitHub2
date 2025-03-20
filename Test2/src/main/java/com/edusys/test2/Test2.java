2e88965aaff70df3b36d254b89265aae42dac7bf
TuanLeGV <tuanlegv2023@gmail.com>
2025-03-20 13:59:27 +0700

    Initials Project

---
+++ Test2/src/main/java/com/edusys/test2/Test2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edusys.test2;
+
+/**
+ *
+ * @author user
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
